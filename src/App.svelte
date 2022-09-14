bc5e44c20d1f9c0600ab8aacbe9187b68d682a06
Jens Krumsieck <jenso666@live.de>
2022-09-14 17:25:03 +0200

    add border

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import Cropper from "svelte-easy-crop";
   import { onMount } from "svelte";
+  import Toggle from "./lib/components/Toggle.svelte";
 
-  let image, original;
+  let image, original, frame, px
   let canvasArray = [];
   let images = [];
   let preview;
+  let isFrame = false;
 
   const files = [
     "endlichmachen.png",
@@ -29,11 +31,13 @@
   onMount(() => {
     //Create canvasses (is that the plural?)
     files.forEach((f) => {
-      createCanvas();  
+      createCanvas();
       let badge = new Image();
       badge.src = "frames/" + f;
       images.push(badge);
     });
+    frame = new Image();
+    frame.src = "frames/frame.svg";
   });
 
   function createCanvas() {
@@ -46,7 +50,10 @@
       "md:h-48",
       "h-24",
       "rounded-full",
-      "cursor-pointer"
+      "cursor-pointer",
+      "hover:rounded-lg",
+      "hover:border-4",
+      "hover:border-yellow"
     );
     preview.append(canvas);
     canvasArray.push(canvas);
@@ -74,7 +81,7 @@
   }
 
   function onCropComplete(e) {
-    let px = e.detail.pixels;
+    px = e.detail.pixels;
     previewCrop(px.width, px.height, px.x, px.y);
   }
 
@@ -86,6 +93,7 @@
       var ctx = canvas.getContext("2d");
 
       ctx.drawImage(original, dx, dy, width, height, 0, 0, width, height);
+      if(isFrame) ctx.drawImage(frame, 0, 0, width, height);
       ctx.drawImage(images[i], 0, 0, width, height);
     }
   }
@@ -94,7 +102,9 @@
 <div class="flex flex-wrap md:w-1/2 container mx-auto bg-green">
   <div class="p-6 flex gap-4">
     <img src="logo.png" alt="logo" class="h-8" />
-    <span class="font-bold text-xl text-yellow italic">Profilbildrahmen Generator</span>
+    <span class="font-bold text-xl text-yellow italic"
+      >Profilbildrahmen Generator</span
+    >
   </div>
   <div class="bg-green relative w-full h-[35vh] md:h-[50vh]">
     {#if image}
@@ -107,36 +117,37 @@
       />
     {/if}
   </div>
-  <div class="p-7">
+  <div class="p-7 flex gap-6">
     <input
       class="text-sm text-white
-    file:mr-5 file:py-2 file:px-6
-    file:rounded-full file:border-0
-    file:text-sm file:font-medium
-    file:bg-orange file:text-black
-    hover:file:cursor-pointer hover:file:bg-yellow
-    hover:file:text-black"
+      file:mr-5 file:py-2 file:px-6
+      file:rounded-full file:border-0
+      file:text-sm file:font-medium
+      file:bg-orange file:text-black
+      hover:file:cursor-pointer hover:file:bg-yellow
+      hover:file:text-black"
       type="file"
       accept=".jpg, .jpeg, .png"
       on:change={(e) => onFileSelected(e)}
     />
-    <ol class="list-decimal text-white font-bold m-6">
-      <li>
-        <strong class="italic my-4"
-          >Auf Schaltfläche tippen und Bild auswählen.</strong
-        >
-      </li>
-      <li>
-        <strong class="italic my-4">Bild zurechtschneiden.</strong>
-      </li>
-      <li>
-        <strong class="italic my-4"
-          >Zum Speichern auf das gewünschte Muster tippen.</strong
-        >
-      </li>
-    </ol>
-    <div id="preview" class="w-full flex flex-wrap gap-4 my-4" />
+    <Toggle bind:isChecked={isFrame} onChange={() => previewCrop(px.width, px.height, px.x, px.y)}>Rahmen um das Bild legen</Toggle>
   </div>
+  <ol class="list-decimal text-white font-bold m-6 px-7">
+    <li>
+      <strong class="italic my-4"
+        >Auf Schaltfläche tippen und Bild auswählen.</strong
+      >
+    </li>
+    <li>
+      <strong class="italic my-4">Bild zurechtschneiden.</strong>
+    </li>
+    <li>
+      <strong class="italic my-4"
+        >Zum Speichern auf das gewünschte Muster tippen.</strong
+      >
+    </li>
+  </ol>
+  <div id="preview" class="w-full flex flex-wrap gap-4 my-4" />
 </div>
 
 <style global lang="postcss">
---
+++ src/lib/components/Toggle.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let isChecked;
+  export let onChange
+</script>
+
+<label for="toogleA" class="flex items-center cursor-pointer">
+  <!-- toggle -->
+  <div class="relative">
+    <!-- input -->
+    <input
+      id="toogleA"
+      type="checkbox"
+      class="sr-only"
+      bind:checked={isChecked}
+      on:change={onChange}
+    />
+    <!-- line -->
+    <div class="w-10 h-4 bg-black rounded-full shadow-inner" />
+    <!-- dot -->
+    <div
+      class="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"
+    />
+  </div>
+  <!-- label -->
+  <div class="ml-3 text-white font-medium"><slot/></div>
+</label>
+
+<style>
+  input:checked ~ .dot {
+    transform: translateX(100%);
+    background-color: #48bb78;
+  }
+</style>
